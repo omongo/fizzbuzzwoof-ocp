4ba276d4aaf114baa9930a4f21db7816b1afcaa1
Chanathip Srithanrat <omongo@gmail.com>
2014-06-09 02:13:41 +0700

    Rename test methods

--- fizzbuzzwoof_test.py
+++ fizzbuzzwoof_test.py
@@ -13,10 +13,12 @@ class TestFizzBuzz(unittest.TestCase):
         expected = 'Fizz'
         actual = FizzBuzzWoofFactory().create().say(3)
         self.assertEqual(expected, actual)
+
     def test_5_should_be_Buzz(self):
         expected = 'Buzz'
         actual = FizzBuzzWoofFactory().create().say(5)
         self.assertEqual(expected, actual)
+
     def test_7_should_be_Woof(self):
         expected = 'Woof'
         actual = FizzBuzzWoofFactory().create().say(7)
@@ -27,17 +29,17 @@ class TestFizzBuzz(unittest.TestCase):
         actual = FizzBuzzWoofFactory().create().say(15)
         self.assertEqual(expected, actual)
 
-    def test_21_should_be_Woof(self):
+    def test_21_should_be_FizzWoof(self):
         expected = 'FizzWoof'
         actual = FizzBuzzWoofFactory().create().say(21)
         self.assertEqual(expected, actual)
 
-    def test_35_should_be_Woof(self):
+    def test_35_should_be_BuzzWoof(self):
         expected = 'BuzzWoof'
         actual = FizzBuzzWoofFactory().create().say(35)
         self.assertEqual(expected, actual)
 
-    def test_105_should_be_Woof(self):
+    def test_105_should_be_FizzBuzzWoof(self):
         expected = 'FizzBuzzWoof'
         actual = FizzBuzzWoofFactory().create().say(105)
         self.assertEqual(expected, actual)
