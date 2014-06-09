8e7a04cefc9659aa86cb8835cc8ceb783e25ce75
Chanathip Srithanrat <omongo@gmail.com>
2014-06-09 10:34:23 +0700

    Change to tuple, and Remove clone operation

--- fizzbuzzwoof.py
+++ fizzbuzzwoof.py
@@ -2,11 +2,11 @@ import rules
 
 class FizzBuzzWoofFactory:
     def create(self):
-        return FizzBuzzWoof([rules.Fizz(), rules.Buzz(), rules.Woof()])
+        return FizzBuzzWoof((rules.Fizz(), rules.Buzz(), rules.Woof()))
 
 class FizzBuzzWoof:
     def __init__(self, rules):
-        self._rules = rules[:]
+        self._rules = rules
     def say(self, number):
         word = ''
         for rule in self._rules:
